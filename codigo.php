a08b00271bee4826c1958a20d7055dd688b8d9a3
Mauricio Orellana <25000174@galileo.edu>
2025-03-24 02:07:03 +0000

    Codigo PHP e imagen creada usando IA generativa

---
+++ codigo.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Hola mundo, soy Mau\n\n";
+?>
+
+<img src="image.jpg" alt="Una-imagen-creada-usando-inteligencia-articial-con-el-prompt-gundam-like-robot-looking-over-the-city-manga-style-sunset-behind-use-rule-of-thirds">
